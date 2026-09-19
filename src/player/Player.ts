ef35be01ebef7e357ce5b520c0ca00d2075da2ed
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2026-09-19 09:11:56 +0400

    Update knight animation frames for new sprite

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -12,7 +12,8 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     this.setScale(0.4);
     // Размеры тела задаются до масштаба: в игре остаётся прежнее тело 40×56.
     this.body.setSize(100, 140);
-    this.body.setOffset(6, 18);
+    // Над головой оставлено место для поднятого меча; тело стоит у ног.
+    this.body.setOffset(10, 58);
     this.body.setCollideWorldBounds(true);
     this.cursors = scene.input.keyboard!.createCursorKeys();
     this.play('knight-idle');
